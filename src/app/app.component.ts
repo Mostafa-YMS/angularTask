18429d07f8cc038db00393a9e2fcf185ac71f870
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-03-03 20:34:36 +0200

    app ts and app test removed unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angularTask';
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-}
+export class AppComponent {}
