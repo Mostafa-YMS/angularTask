bc0e4f9d4bfc04b991cfc5273528b16717bfdf24
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-03-03 19:34:34 +0200

    Remove user tested

--- src/app/remove-user/remove-user.component.spec.ts
+++ src/app/remove-user/remove-user.component.spec.ts
@@ -4,6 +4,7 @@ import { RemoveUserComponent } from './remove-user.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UsersService } from '../users.service';
 
 describe('RemoveUserComponent', () => {
   let component: RemoveUserComponent;
@@ -11,10 +12,10 @@ describe('RemoveUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:[MatDialogModule, HttpClientModule, RouterTestingModule],
-      declarations: [ RemoveUserComponent ]
-    })
-    .compileComponents();
+      imports: [MatDialogModule, HttpClientModule, RouterTestingModule],
+      declarations: [RemoveUserComponent],
+      providers: [{ provide: UsersService, useClass: UsersServiceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -27,3 +28,9 @@ describe('RemoveUserComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+class UsersServiceStub {
+  deleteUser(id: String) {
+    return { status: 204 };
+  }
+}
