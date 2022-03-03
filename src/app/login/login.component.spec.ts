6f7d49f426be73b966d3d359a8c96976ca2540e8
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-03-03 19:26:32 +0200

    Login unnecessary test removed

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { Location } from '@angular/common';
 import { UsersService } from '../users.service';
 
 describe('LoginComponent', () => {
@@ -47,11 +46,6 @@ describe('LoginComponent', () => {
   it('should contain two inputs', () => {
     const inputs = fixture.debugElement.queryAll(By.css('input'));
     expect(inputs.length).toBe(2);
-  });
-
-  it('', () => {
-    const location = fixture.debugElement.injector.get(Location);
-    expect(location.path()).toBe('');
   });
 });
 
