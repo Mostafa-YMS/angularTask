45b6733ab86b0291dd1c1fbfeb8afcf58300628f
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-03-03 19:24:52 +0200

    Navbar test

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -3,6 +3,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
+
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -10,10 +14,15 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:[HttpClientModule, RouterTestingModule],
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientModule,
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: DummyComponent },
+          { path: 'register', component: DummyComponent },
+        ]),
+      ],
+      declarations: [NavbarComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -25,4 +34,61 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain span contain User Admin', () => {
+    const span = fixture.debugElement.query(By.css('span'));
+    expect(span.nativeElement.textContent).toBe('User Admin');
+  });
+
+  it('should contain one span', () => {
+    const span = fixture.debugElement.queryAll(By.css('span'));
+    expect(span.length).toBe(1);
+  });
+
+  it('should contain three anchor tags', () => {
+    const a = fixture.debugElement.queryAll(By.css('a'));
+    expect(a.length).toBe(2);
+  });
+
+  it('should contain one anchor tag after login', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    const a = fixture.debugElement.queryAll(By.css('a'));
+    expect(a.length).toBe(1);
+  });
+
+  it('should contain one button after login', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    const button = fixture.debugElement.queryAll(By.css('button'));
+    expect(button.length).toBe(1);
+  });
+
+  it('path should be login after click Login anchor', () => {
+    const location = fixture.debugElement.injector.get(Location);
+    const a = fixture.debugElement.queryAll(By.css('a'));
+    expect(a[0].nativeElement.innerText).toBe('Login')
+    a[0].nativeElement.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/login');
+    });
+  });
+
+  it('path should be register after click Register anchor', () => {
+    const location = fixture.debugElement.injector.get(Location);
+    const a = fixture.debugElement.queryAll(By.css('a'));
+    expect(a[1].nativeElement.innerText).toBe('Register')
+    a[1].nativeElement.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/register');
+    });
+  });
+  
 });
+
+
+
+@Component({ template: '' })
+class DummyComponent {}
